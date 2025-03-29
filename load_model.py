efdfed602b8070a4ccf66cb62ad189d2bd9a839e
cheo2322 <barcecheo2319@gmail.com>
2025-03-29 00:08:50 -0500

    Fix TODOs

--- load_model.py
+++ load_model.py
@@ -1,12 +1,13 @@
 import mlflow
 import numpy as np
+import pandas as pd
 
-# TODO: Set tht MLFlow server uri
-uri = ___________________
+# Set tht MLFlow server uri
+uri = "http://127.0.0.1:6001"
 mlflow.set_tracking_uri(uri=uri)
 
-# TODO: Provide model path/url
-logged_model = ___________________
+# Provide model path/url
+logged_model = "mlflow-artifacts:/679920279417233199/9b5c08367c17433298a6b9bc2493e640/artifacts/model_artifacts"
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.sklearn.load_model(logged_model)
@@ -15,8 +16,9 @@ loaded_model = mlflow.sklearn.load_model(logged_model)
 np.random.seed(42)
 data = np.random.rand(1, 64)
 
-# TODO: Predict the output for the data. You might need to use a pandas DataFrame due to a constraint from MLFlow.
-prediction = loaded_model.predict(___________)
+# Predict the output for the data. You might need to use a pandas DataFrame due to a constraint from MLFlow.
+data_df = pd.DataFrame(data)
+prediction = loaded_model.predict(data_df)
 
 # Print out prediction result
 print(prediction)
--- train_model.py
+++ train_model.py
@@ -6,18 +6,18 @@ from mlflow.models import infer_signature
 
 from utility import pipeline
 
-# TODO: Set tht MLFlow tracking server uri
-uri = _________
+# Set tht MLFlow tracking server uri
+uri = "http://127.0.0.1:6001"
 # Use mlflow.set_tracking_uri to set the uri
 mlflow.set_tracking_uri(uri)
 
 # Set experiment name
-email = "______@andrew.cmu.edu"  # TODO: Use a customized experiment name
+email = "sergio.hidalgo@yachaytech.edu.ec"  # Use a customized experiment name
 experiment_name = f"{email}-lab7"
 mlflow.set_experiment(experiment_name)
 
-# TODO: Generates train and test dataset using `pipeline.data_preprocessing` function
-X_train, X_test, y_train, y_test = ________
+# Generates train and test dataset using `pipeline.data_preprocessing` function
+X_train, X_test, y_train, y_test = pipeline.data_preprocessing()
 
 params = {
     "solver": "lbfgs",
@@ -26,10 +26,10 @@ params = {
     "random_state": 8888,
 }
 
-# TODO: Use `pipeline.train_logistic_regression` to generate trained model
-trained_model = __________________
-# TODO: use `pipeline.evaluation` to evaluate the model
-accuracy = _____________________
+# Use `pipeline.train_logistic_regression` to generate trained model
+trained_model = pipeline.train_logistic_regression(X_train, y_train, params)
+# use `pipeline.evaluation` to evaluate the model
+accuracy = pipeline.evaluation(trained_model, X_test, y_test)
 
 # Log model and metrics to tracking serverhost
 # Start an MLflow run
@@ -43,8 +43,8 @@ with mlflow.start_run(run_name=run_name):
     # Log the model
     model_info = mlflow.sklearn.log_model(
         sk_model=trained_model,
-        artifact_path=None,  # TODO: Set the artifact path appropriately as a string
+        artifact_path="model_artifacts",  # Set the artifact path appropriately as a string
         signature=signature,
         input_example=X_train,
-        registered_model_name=pipeline.generate_model_name(),  # Optional TODO: Replace with a static name if needed - you will need to use the same static name when updating to a newer version of the model
+        registered_model_name=pipeline.generate_model_name(),  # Optional Replace with a static name if needed - you will need to use the same static name when updating to a newer version of the model
     )
